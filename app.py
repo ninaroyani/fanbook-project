4ad512c1d1be46927cdcd82c6a44b41ba15dbb8c
Ninaroyani <ninaroyani2006@gmail.com>
2023-03-28 16:13:36 +0700

    First Commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("MONGODB_URL")
+MONGODB_URI = os.environ.get("MONGODB_URI")
 DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient[MONGODB_URI]
